e563e88f8ecccf0efe8a5b6a4a3dd14049810674
Guilherme Roncaratti Galanti <galanti_imp@hotmail.com>
2025-04-22 22:19:16 -0300

    adding some endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,19 @@
-console.log("hello world");
+import fastify from "fastify";
+
+const server = fastify({ logger: true });
+
+server.get('/teams', async (request, response) => {
+    response.type('application/json').code(200);
+
+    return [{ id: 1, name: 'ferrari' }];
+});
+
+server.get('/drivers', async (request, response) => {
+    response.type('application/json').code(200);
+
+    return [{ id:1, name: 'Max Verstappen', team: 'Red Bull Racing' }];
+});
+
+server.listen({port: Number(process.env.PORT)}, () => {
+    console.log('Server init');
+});
